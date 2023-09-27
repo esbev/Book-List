e7340a4203f3d6d820eb9ae368bab8c90a5202d3
Eric <esbev@hotmail.com>
2023-09-26 20:29:37 -0500

    working on the same

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -61,6 +61,7 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
+    
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
@@ -73,6 +74,7 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook(bookToSave, token);
+      
 
       if (!response.ok) {
         throw new Error('something went wrong!');
